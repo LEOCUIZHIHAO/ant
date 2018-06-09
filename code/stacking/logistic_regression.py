1aa7e939fc2e87f48abacaa7c7c790b773ee942c
UTSAccount <zhenhua.zhang@student.uts.edu.au>
2018-06-09 13:10:22 +1000

    fix stack_split problem

--- code/stacking/logistic_regression.py
+++ code/stacking/logistic_regression.py
@@ -5,32 +5,53 @@ import math
 def stack_split(features,labels,number_of_model):
     # Define number of sizes per model
     fold_size = int(labels.size/number_of_model)
+    print(labels.size)
     # Iterate number of models to get different fold, feature and label data
     fold_split = {}
     feature_split = {}
     label_split = {}
+
+    end_row = fold_size - 1
     for i in range(number_of_model):
         # define starting and end rows of the fold data
-        start_row = fold_size * i
-        end_row = start_row + fold_size
-        print("\nfold_{}".format(i+1) + "starting between row:{}".format(start_row) + " and row:{}".format(end_row))
-        # Store extrated fold data from feature
-        fold_split["fold_{}".format(i+1)] = features[start_row:end_row,:]
-        # Delete the extrated data from feature and label data
-        feature_split["feature_{}".format(i+1)] = np.delete(features, np.s_[start_row:(start_row + fold_size)], axis = 0)
-        label_split["label_{}".format(i+1)] = np.delete(labels, np.s_[start_row:(start_row + fold_size)], axis = 0)
+        if i != number_of_model -1:
+            if i == 0:
+                start_row = 0
+            else:
+                start_row = end_row + 1
+                end_row = start_row + fold_size - 1
+            print("\nfold_{}".format(i+1) + " starting between row:{}".format(start_row) + " and row:{}".format(end_row))
+            print(fold_size)
+            # Store extrated fold data from feature
+            fold_split["fold_{}".format(i+1)] = features[start_row:end_row,:]
+            # Delete the extrated data from feature and label data
+            feature_split["feature_{}".format(i+1)] = np.delete(features, np.s_[start_row:(start_row + fold_size)], axis = 0)
+            label_split["label_{}".format(i+1)] = np.delete(labels, np.s_[start_row:(start_row + fold_size)], axis = 0)
+        else:
+            start_row = end_row + 1
+            row = start_row
+            print("\nfold_{}".format(i+1) + " starting between row:{}".format(start_row))
+            # Store extrated fold data from feature
+            fold_split["fold_{}".format(i+1)] = features[start_row:,:]
+            # Delete the extrated data from feature and label data
+            feature_split["feature_{}".format(i+1)] = np.delete(features, np.s_[start_row:], axis = 0)
+            label_split["label_{}".format(i+1)] = np.delete(labels, np.s_[start_row:], axis = 0)
+
     print("\nEnd of split, acess via ['fold_number'], ['feature_number'] and ['label_number']")
+    print(len(fold_split["fold_5"]) + row)
+    exit()
     return fold_split, feature_split, label_split
 
 def stack_logistic(features,labels):
     fold_split, feature_split, label_split = stack_split(features,labels,5)
     scores = []
     for i in range(len(fold_split)):
-        print("\n")
+        print("\nProcessing logistic model number:{}".format(i+1))
         logistic = LogisticRegression()
         logistic.fit(feature_split["feature_{}".format(i+1)], label_split["label_{}".format(i+1)])
-        stack_score = logistic.predic_proba(fold_split["fold_{}".format(i+1)])
-        scores = scores.append(stack_score)
+        stack_score = logistic.predict_proba(fold_split["fold_{}".format(i+1)])
+        scores = scores + stack_score[:,0].tolist()
+        print(scores)
     return scores
 
 
