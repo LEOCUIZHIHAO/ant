e294fc5faaf79274b0570493b83da4ef5ab90d44
UTSAccount <zhenhua.zhang@student.uts.edu.au>
2018-06-24 16:26:22 +1000

    Added program to add date as an feature

---
+++ code/preprocessing/add_feature_time.py
@@ -0,0 +1,27 @@
+import pandas as pd
+import numpy as np
+
+train_path = ""
+new_train_save_path = ""
+test_path = ""
+new_test_save_path = ""
+
+def convert_to_date(array):
+    list = []
+    for i in range(len(array)):
+        value = array[i] % 100
+        list.append(value)
+    return list
+
+def main():
+    train_data = pd.read_csv(train_path)
+    train_data_new_feature = convert_to_date(train_data["date"])
+    train_data['f298'] = train_data_new_feature
+    train_data.to_csv(new_train_save_path)
+
+    test_data = pd.read_csv(test_path)
+    test_data_new_feature = convert_to_date(test_data["date"])
+    test_data['f298'] = test_data_new_feature
+    test_data.to_csv(new_test_save_path)
+
+main()
---
+++ code/preprocessing/save_process_test.py
@@ -0,0 +1,40 @@
+import xgboost as xgb
+from xgboost import XGBClassifier
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import os
+import math
+from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import ExtraTreesClassifier
+from sklearn.svm import SVC
+from sklearn.neural_network import MLPClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.preprocessing import Imputer, StandardScaler, Normalizer
+from sklearn.feature_selection import SelectFromModel
+from sklearn.externals import joblib
+from sklearn.decomposition import PCA
+
+import datetime
+
+
+
+def progress_log(names, classifiers):
+    now = datetime.datetime.now()
+    for name, clf in zip(names, classifiers):
+        with open("log/" + "{}_".format(now.year)+"{}_".format(now.month)+"{}".format(now.day), "a") as f:
+            f.write(str('*'*10) + str(name) + str('*'*10) + '\n')
+            f.write(str(clf) + "\n")
+
+# def progress_log():
+
+
+def main():
+    names = ["random_forest", "LogisticRegression"]
+    classifiers = [RandomForestClassifier(n_estimators = 450, max_depth = 4, criterion='entropy'),
+                   LogisticRegression(class_weight = "balanced")]
+    progress_log(names,classifiers)
+    # value = pd.DataFrame(clf)
+if __name__ == "__main__":
+    main()
